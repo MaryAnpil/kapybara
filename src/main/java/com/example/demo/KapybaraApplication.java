492a48e3f23b006d9e3865d178259bb01a50544e
MaryAnpil <scarbroad@yandex.ru>
2023-02-14 21:47:41 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/KapybaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KapybaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KapybaraApplication.class, args);
+	}
+
+}
